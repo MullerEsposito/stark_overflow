a91937503b211ea7f43d92252d1877d3c3e4cab2
Arnaelcio <arnaelcio.gomes@gmail.com>
2025-07-31 19:01:34 -0300

    using vite preview

--- frontend/react/cypress/support/constants.ts
+++ frontend/react/cypress/support/constants.ts
@@ -4,5 +4,5 @@ export const SELECTORS = {
   GET_ALL_FORUMS: '0x17cc439fbc04c207fe4f86321ef047d708a1e2b8b303fe943cb9461673a5496'  // Adicione outros seletores de função aqui conforme necessário
 };
 
-export const MOCK_ACCOUNT ='0x045fd9335f323ab646ee6fea36ec88ebb4a130a68289cb074f8221478fcb99b6';
+export const MOCK_ACCOUNT ='0x04f348398f859a55a0c80b1446c5fdc37edb3a8478a32f10764659fc241027d3';
 export const MOCK_CHAIN_ID = '0x534e5f474f45524c49';
