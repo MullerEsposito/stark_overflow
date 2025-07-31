923bae31d5852a8c578ebceda917d69d89993e74
Arnaelcio <arnaelcio.gomes@gmail.com>
2025-07-31 00:04:53 -0300

    adjustments

--- frontend/react/cypress/support/constants.ts
+++ frontend/react/cypress/support/constants.ts
@@ -1,4 +1,4 @@
-export const RPC_URL = 'https://starknet-sepolia.public.blastapi.io/rpc/v0_7';
+export const RPC_URL = 'http://localhost:5050';
 export const SELECTORS = {
   GET_ALL_FORUMS: '0x17cc439fbc04c207fe4f86321ef047d708a1e2b8b303fe943cb9461673a5496'  // Adicione outros seletores de função aqui conforme necessário
 };
