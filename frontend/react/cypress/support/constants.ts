9ad24073aacc2cf4b7bbf9774feb42d93a8782c0
Arnaelcio <arnaelcio.gomes@gmail.com>
2025-07-31 03:12:04 -0300

    fix account

--- frontend/react/cypress/support/constants.ts
+++ frontend/react/cypress/support/constants.ts
@@ -4,5 +4,5 @@ export const SELECTORS = {
   GET_ALL_FORUMS: '0x17cc439fbc04c207fe4f86321ef047d708a1e2b8b303fe943cb9461673a5496'  // Adicione outros seletores de função aqui conforme necessário
 };
 
-export const MOCK_ACCOUNT = '0x064b48806902a367c8598f4f95c305e8c1a1acba5f082d294a43793113115691';
+export const MOCK_ACCOUNT = Cypress.env('VITE_CONTRACT_ADDRESS');
 export const MOCK_CHAIN_ID = '0x534e5f474f45524c49';
