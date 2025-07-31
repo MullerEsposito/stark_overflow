f200e1a91c6536d406237ecd7ed408f68e90f8c2
Arnaelcio <arnaelcio.gomes@gmail.com>
2025-07-31 03:14:48 -0300

    fix env

--- frontend/react/cypress/support/constants.ts
+++ frontend/react/cypress/support/constants.ts
@@ -4,5 +4,5 @@ export const SELECTORS = {
   GET_ALL_FORUMS: '0x17cc439fbc04c207fe4f86321ef047d708a1e2b8b303fe943cb9461673a5496'  // Adicione outros seletores de função aqui conforme necessário
 };
 
-export const MOCK_ACCOUNT = Cypress.env('VITE_CONTRACT_ADDRESS');
+export const MOCK_ACCOUNT = Cypress.env('CONTRACT_ADDRESS');
 export const MOCK_CHAIN_ID = '0x534e5f474f45524c49';
