e5ce8e599b04c091316d1e05e62e08c4162eb7b7
jimenezz22 <ljimenezbogantes@gmail.com>
2025-08-29 20:49:38 -0600

    feat: add vitest and react testing library setup for unit tests

--- frontend/react/vite.config.ts
+++ frontend/react/vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest" />
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { fileURLToPath } from 'url';
@@ -16,6 +17,24 @@ export default defineConfig({
       "@utils": fileURLToPath(new URL("src/utils", import.meta.url)),
       "@hooks": fileURLToPath(new URL("src/hooks", import.meta.url)),
       "@app-types": fileURLToPath(new URL("src/types", import.meta.url)),
+    },
+  },
+  test: {
+    environment: 'jsdom',
+    setupFiles: './tests/setup.ts',
+    globals: true,
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'json', 'html'],
+      exclude: [
+        'node_modules/',
+        'tests/',
+        '*.config.ts',
+        '**/*.d.ts',
+        '**/*.config.*',
+        '**/mockData',
+        'src/main.tsx',
+      ],
     },
   },
 });
